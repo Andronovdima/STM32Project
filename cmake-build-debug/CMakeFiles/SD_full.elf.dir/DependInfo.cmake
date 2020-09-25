6380b3622dfeb9ff576814949ba1fedcb4987379
Andronovdima <hifromnorway@gmail.com>
2020-09-25 15:26:53 +0300

    add project

---
+++ cmake-build-debug/CMakeFiles/SD_full.elf.dir/DependInfo.cmake
@@ -0,0 +1,73 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/andronovdima/Desktop/Stm32/SD_full/startup/startup_stm32f103xb.s" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/startup/startup_stm32f103xb.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../sdcard"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_spi.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_spi.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Src/main.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Src/main.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Src/sd.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Src/sd.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Src/stm32f1xx_hal_msp.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Src/stm32f1xx_hal_msp.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Src/stm32f1xx_it.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Src/stm32f1xx_it.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Src/syscalls.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Src/syscalls.c.obj"
+  "/Users/andronovdima/Desktop/Stm32/SD_full/Src/system_stm32f1xx.c" "/Users/andronovdima/Desktop/Stm32/SD_full/cmake-build-debug/CMakeFiles/SD_full.elf.dir/Src/system_stm32f1xx.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../sdcard"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SD_full.elf.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_spi.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Src/main.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Src/sd.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/SD_full.elf.dir/Src/system_stm32f1xx.c.obj"
+  "CMakeFiles/SD_full.elf.dir/startup/startup_stm32f103xb.s.obj"
+  "SD_full.elf"
+  "SD_full.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/SD_full.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
